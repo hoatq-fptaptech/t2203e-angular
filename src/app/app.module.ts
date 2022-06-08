f368394d11dd21c81ac16c1e9746daf607a3d631
billytrinh <quanghoa.trinh@gmail.com>
2022-06-08 18:38:37 +0700

    DTO demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {StudentComponent} from "./student/student.component";
 import {ClassroomComponent} from "./classroom/classroom.component";
+import {ProductsComponent} from "./products/products.component";
+import {ProductComponent} from "./product/product.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentComponent,
-    ClassroomComponent
+    ClassroomComponent,
+    ProductsComponent,ProductComponent
   ],
   imports: [
     BrowserModule
